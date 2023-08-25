fb90681aed7ba41ffe5a9c5b5c6898dce12cbf71
Justus Bogner <justus.bogner@iste.uni-stuttgart.de>
2023-08-25 10:31:24 +0200

    Updates

---
+++ d0447323-c8c35bf518f45ce0fa03.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjreb_cv=self.webpackChunkjreb_cv||[]).push([[415],{9463:function(t,e,r){r.d(e,{S4z:function(){return c}});var a=r(1046);function c(t){return(0,a.w_)({tag:"svg",attr:{role:"img",viewBox:"0 0 24 24"},child:[{tag:"title",attr:{},child:[]},{tag:"path",attr:{d:"M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"}}]})(t)}}}]);
+//# sourceMappingURL=d0447323-c8c35bf518f45ce0fa03.js.map

ec45188874b3212c9230fb50decc81f650af83fa
xJREB <justus.bogner@gmail.com>
2024-09-03 10:58:26 +0200

    Updates

---
+++ d9067523-b129a4f1564c5f310460.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjreb_cv=self.webpackChunkjreb_cv||[]).push([[945],{712:function(t,c,e){e.d(c,{mO$:function(){return n}});var r=e(2225);function n(t){return(0,r.k5)({tag:"svg",attr:{role:"img",viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M5.242 13.769L0 9.5 12 0l12 9.5-5.242 4.269C17.548 11.249 14.978 9.5 12 9.5c-2.977 0-5.548 1.748-6.758 4.269zM12 10a7 7 0 1 0 0 14 7 7 0 0 0 0-14z"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=d9067523-b129a4f1564c5f310460.js.map
